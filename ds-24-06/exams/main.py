8d4a0a06b887f34aa100e68af8af557f40ffde68
TheBandik <shnr.arky@gmail.com>
2025-03-15 13:56:50 +1000

    add exams

---
+++ ds-24-06/exams/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory='templates')
+
+class Task(BaseModel):
+    title: str
+
+tasks = []
+
+@app.get('/', response_class=HTMLResponse)
+def get_index(request: Request):
+    return templates.TemplateResponse(
+        'index.html',
+        {'request': request, 'tasks': tasks}
+    )
+
+@app.post('/tasks', response_model=Task, status_code=201)
+def create_task(request: Request, task_title: str = Form(...)):
+    tasks.append(Task(title=task_title))
+    return RedirectResponse(url="/", status_code=303)
+
+@app.post('/tasks/delete/{task_id}', status_code=204)
+def delete_task(task_id: int):
+    if task_id >= len(tasks):
+        raise HTTPException(status_code=404, detail="Task not found")
+    tasks.pop(task_id)
+    return RedirectResponse(url="/", status_code=303)
