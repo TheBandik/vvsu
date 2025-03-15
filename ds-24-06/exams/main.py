c38b1e8f35887d62bf6b744a9913413f1f336431
TheBandik <shnr.arky@gmail.com>
2025-03-15 14:15:45 +1000

    update exams

--- ds-24-06/exams/main.py
+++ ds-24-06/exams/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory='templates')
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 class Task(BaseModel):
     title: str
